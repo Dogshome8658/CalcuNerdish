81ace5a785775e2db293b49a92089929511deede
Dogshome8658 <133223933+Dogshome8658@users.noreply.github.com>
2025-05-27 23:22:13 +0800

    Wanna sleep

--- COMPAccurateNumberImplementation.h
+++ COMPAccurateNumberImplementation.h
@@ -1,16 +1,29 @@
 #ifndef COMPANI
+
 struct AccurateNumber{
+    char IAmDesNum
     char Places[100];
     char IfIsPositive=1;
     char DesPlaces[100];
 };
 
 struct Fraction{
-    AccurateNumber UpperNumber;
-    AccurateNumber LowerNumber;
-    char IfIsPositive=1;//Ignore both?
+    char IAmFraction =1
+    struct AccurateNumber UpperNumber;
+    struct AccurateNumber LowerNumber;
+    //char IfIsPositive=1;//Ignore both?
+    //I guess just do some math
 };
 
 #define Number AccurateNumber
+#define bool char
+
+#define false 0
+#define true 1
+
+/*#define DesNumber_T 0
+#define AccrateNumber_T 0
+#define Fraction_T 1*/
+
 #endif
 #define COMPANI 1
--- COMPFourBasicCalculation.h
+++ COMPFourBasicCalculation.h
@@ -3,10 +3,26 @@
 
 int ErrorExecuted=0;
 
-#define Overflow_E -1
+#define Overflow_E 1
+#define Underflow_E -1
 
-bool Add(Number FirstNumber,Number SecondNumber,Number* Result){
+bool Add_SinglePlace(char FirstPlace,char SecondPlace,char* Result,bool* NeedAddTen){
+    char PreResult=FirstPlace+SecondPlace;
+    if(PreResult>=10){
+        (&NeedAddTen)=true;
+    }else{
+        (&NeedAddTen)=false;
+    }
+    (&Result)=PreResult%10;
+    return true;
+}
 
+bool Add_Des(Number FirstNumber,Number SecondNumber,Number* Result){
+    if(FirstNumber.IfIsPositive==true&&SecondNumber.IfIsPositive==true){
+        bool IfIhadToAddOneOnTheNextPlace=false;
+        
+    }
+    
 }
 
 #endif
